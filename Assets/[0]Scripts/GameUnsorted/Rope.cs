a192e49908c1a2f2c545275773a8e2a8eaaaf7a7
southnort <south-nort@ya.ru>
2023-05-13 23:37:24 +0300

    настройка джоинтов,  Yrr пакет

---
+++ Assets/[0]Scripts/GameUnsorted/Rope.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Default
+{
+    internal sealed class Rope : MonoBehaviour
+    {
+        public int PointsCount;
+        public LineRenderer LineRend;
+        public float PointsDistance;
+        public float PointColliderRadius;
+
+        List<Rigidbody> GeneratedPoints;
+
+        public Transform PointA;
+        public Transform PointB;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GeneratedPoints = new List<Rigidbody>();
+
+            Vector3 Start = PointA.position;
+            Vector3 End = PointB.position;
+            Vector3 Dir = End - Start;
+
+            for (int i = 0; i < PointsCount; i++)
+            {
+                GameObject ChainPoint = new GameObject();
+                ChainPoint.transform.SetParent(transform);
+                SphereCollider SC = ChainPoint.AddComponent<SphereCollider>();
+                SC.radius = PointColliderRadius;
+                Rigidbody RB = ChainPoint.AddComponent<Rigidbody>();
+                GeneratedPoints.Add(RB);
+                RB.drag = 1;
+                if (i == 0 || i == PointsCount - 1)
+                {
+                    RB.isKinematic = true;
+                }
+                ChainPoint.layer = LayerMask.NameToLayer("Rope");
+                ChainPoint.transform.position = Start + Dir * ((float)i / (float)PointsCount);
+                if (i > 0)
+                {
+                    ConfigurableJoint CJ = ChainPoint.AddComponent<ConfigurableJoint>();
+                    CJ.connectedBody = GeneratedPoints[i - 1];
+                    CJ.xMotion = ConfigurableJointMotion.Limited;
+                    CJ.yMotion = ConfigurableJointMotion.Limited;
+                    CJ.zMotion = ConfigurableJointMotion.Limited;
+
+                    SoftJointLimit SJL = CJ.linearLimit;
+                    SJL.limit = PointsDistance;
+                    SJL.bounciness = 1;
+                    SoftJointLimitSpring SJLS = CJ.linearLimitSpring;
+                    SJLS.damper = 3;
+                    SJLS.spring = 999;
+                    CJ.linearLimit = SJL;
+                    CJ.linearLimitSpring = SJLS;
+
+                    CJ.autoConfigureConnectedAnchor = false;
+                    CJ.connectedAnchor = Vector3.zero;
+                }
+            }
+            LineRend.positionCount = PointsCount;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (int i = 0; i < GeneratedPoints.Count; i++)
+            {
+                LineRend.SetPosition(i, GeneratedPoints[i].transform.position);
+                if (i == 0)
+                {
+                    GeneratedPoints[i].transform.position = PointA.position;
+                }
+                else if (i == GeneratedPoints.Count - 1)
+                {
+                    GeneratedPoints[i].transform.position = PointB.position;
+                }               
+            }
+        }
+    }
+}
